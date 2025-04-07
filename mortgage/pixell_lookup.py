a1b6c3bad2a50b21264a0a0d41a1601abaf9117d
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-04-06 22:12:02 -0500

    Pixell_lookup.py modification: Added an Enum Mortgage rate and payment frequency class.

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -14,3 +14,21 @@ from enum import Enum
 
 VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
 
+class MortgageRate(Enum):
+    """
+    Enumeration of Pixell rivers mortgage rates. 
+    """
+    FIXED_5: 0.0519
+    FIXED_3: 0.0589
+    FIXED_1: 0.0599
+    VARIABLE_5: 0.0649
+    VARIABLE_3: 0.0669
+    VARIABLE_1: 0.0679
+
+class PaymentFrequency(Enum):
+    """
+    Enumeration of mortgage payment frequency
+    """
+    MONTHLY: 12
+    BI_WEEKLY: 26
+    WEEKLY: 52
